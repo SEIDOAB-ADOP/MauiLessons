f347a9de98305f59c550f2a39ee6878c316fce82
Martin <martin_lenart@icloud.com>
2026-01-19 11:09:47 +0100

    update

--- MauiLessons/MainPage.xaml.cs
+++ MauiLessons/MainPage.xaml.cs
@@ -8,5 +8,13 @@
         {
             InitializeComponent();
         }
+
+        protected override void OnNavigatedTo(NavigatedToEventArgs args)
+        {
+            base.OnNavigatedTo(args);
+
+            //Routing of this page
+            Title += $"   ({Shell.Current.CurrentState.Location.ToString()})";
+        }
     }
 }
